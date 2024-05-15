9669da754d848fdfb9b09effa62f459d4a397bab
Joseph Mumford <josephamumford@gmail.com>
2024-05-14 21:49:05 -0500

    add console input

--- main.go
+++ main.go
@@ -1,13 +1,41 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
+
 	"github.com/JosephAMumford/image-reader/formats"
 )
+
+const PROMPT = "> "
 
 func main() {
 
 	bmpFile := formats.BMP{}
 
 	bmpFile.LoadFile("samples/cat-dragon.bmp")
-	bmpFile.Render("\u2588")
+
+	scanner := bufio.NewScanner(os.Stdin)
+
+	fmt.Println("Enter any single character to display the image. Type 'quit' to quit")
+
+	for {
+		fmt.Fprint(os.Stdout, PROMPT)
+		scanned := scanner.Scan()
+
+		if !scanned {
+			return
+		}
+
+		line := scanner.Text()
+
+		if line == "quit" {
+			break
+		}
+
+		if len(line) == 1 {
+			bmpFile.Render(line)
+		}
+	}
 }
